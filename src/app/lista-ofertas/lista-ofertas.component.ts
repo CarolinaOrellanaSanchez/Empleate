7cb6f234cbeb828994b121ad12ec3843669b4fc3
Carolina Orellana <carol.orellana.vk@gmail.com>
2017-05-03 14:21:23 +0200

    Creamos la lista de las ofertas y anadimos 2 para ir probando

--- src/app/lista-ofertas/lista-ofertas.component.ts
+++ src/app/lista-ofertas/lista-ofertas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from "../oferta"
 
 @Component({
   selector: 'app-lista-ofertas',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-ofertas.component.css']
 })
 export class ListaOfertasComponent implements OnInit {
+  listaOfertas: Array<Oferta> = [new Oferta('http://comenzandodecero.com/wp-content/uploads/2013/04/Linkedin-para-buscar-trabajo.jpg', 'Telefonica', 'Maquetador web', '03/05/2017', 'Madrid', 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], '13.000€', 'Jornada completa'), new Oferta('http://comenzandodecero.com/wp-content/uploads/2013/04/Linkedin-para-buscar-trabajo.jpg', 'Telefonica', 'Maquetador web', '03/05/2017', 'Madrid', 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], '13.000€', 'Jornada completa')];
 
   constructor() { }
 
