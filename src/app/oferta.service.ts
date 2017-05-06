8467a657f9202b3730e3570ba41d76fd7524f1d7
CarolinaOrellanaSanchez <carol.orellana.vk@gmail.com>
2017-05-06 09:31:40 +0200

    Damos estilos a la oferta detallada

--- src/app/oferta.service.ts
+++ src/app/oferta.service.ts
@@ -4,8 +4,8 @@ import { Oferta } from './oferta';
 @Injectable()
 export class OfertaService {
   listaOfertas: Array < Oferta > = [
-    new Oferta('../../../assets/telefonica.png', 'Telefonica', 'Maquetador web', '03/05/2017', 'Madrid', 'Grado Medio informática', 'Min dos años', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', 3, 'Larga duración', '13.000€', 'Jornada completa'),
-    new Oferta('../../../assets/telefonica.png', 'Telefonica', 'Maquetador web', '03/05/2017', 'Madrid', 'Grado Medio informática', 'Min dos años', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', 3, 'Larga duración', '13.000€', 'Jornada completa')
+    new Oferta('../../../assets/telefonica1.png', 'Telefonica', 'Maquetador web', '1 hora', 'Madrid', 'Grado Medio informática', 'Min dos años', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', 3, 'Larga duración', '13.000€', 'Jornada completa', ['hola', 'hola']),
+    new Oferta('../../../assets/telefonica1.png', 'Grupo Agio', 'Perfiles Java: Coordinador equipo y AP J2ee', '3 horas', 'Barcelona', 'Grado Medio informática', 'Min dos años', ['CSS', 'HTML5', 'Photoshop', 'Illustrator.'], 'Necesitamos maquetador web para el desarrollo de contenido e-learning.', 3, 'Larga duración', '13.000€', 'Jornada completa', ['hola', 'hola'])
   ];
   constructor() { }
  
--- src/app/oferta.ts
+++ src/app/oferta.ts
@@ -1,4 +1,4 @@
 export class Oferta {
-    constructor(public imagenUrl: string, public nombreEmpresa: string, public puesto: string, public fecha: string,  public lugar: string,  public estudios: string,  public experiencia: string, public skills: Array<string>,public descripcion: string,  public vacantes: number,  public duracion: string, public salario: string, public horario: string) {
+    constructor(public imagenUrl: string, public nombreEmpresa: string, public puesto: string, public fecha: string,  public lugar: string,  public estudios: string,  public experiencia: string, public skills: Array<string>,public descripcion: string,  public vacantes: number,  public duracion: string, public salario: string, public horario: string, public habilidades: Array<string>) {
     }
 }
