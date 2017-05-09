3d71d04a8e70e822ca3112d72c9b53796fb1d5dd
Carolina Orellana <carol.orellana.vk@gmail.com>
2017-05-09 14:39:48 +0200

    Cursos nuevos

--- src/app/lista-ofertas/lista-ofertas.component.ts
+++ src/app/lista-ofertas/lista-ofertas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Oferta } from '../oferta';
 import { Router } from '@angular/router';
 import { OfertaService } from '../oferta.service';
@@ -11,6 +11,7 @@ import { OfertaService } from '../oferta.service';
 export class ListaOfertasComponent implements OnInit {
   listaOfertas: Array < Oferta > = [];
   puesto: string;
+  @Input() adminstrador = false;
 
   constructor(private router: Router, private ofertaService: OfertaService) {}
 
