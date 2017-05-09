a81ff6aa1d0b6bddf8655f53b57a6e50111d417b
ALVARO AGUILAR HERVAS <alvaro@MacBook-Pro-de-ALVARO.local>
2017-05-09 09:46:46 +0200

    funcionalidad formulario

--- src/app/formulario-adm/formulario-adm.component.ts
+++ src/app/formulario-adm/formulario-adm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-adm',
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-adm.component.css']
 })
 export class FormularioAdmComponent implements OnInit {
+  miFormulario: FormGroup;
 
-  constructor() { }
+  constructor() {this.miFormulario = new FormGroup({
+
+'puesto': new FormControl('', Validators.required),
+'empresa': new FormControl('', Validators.required),
+'lugar': new FormControl('', Validators.required),
+'estudios_minimos': new FormControl('', Validators.required),
+'experiencia': new FormControl('', Validators.required),
+'conocimientos_necesarios': new FormControl('', Validators.required),
+'descripcion_empresa': new FormControl('', Validators.required),
+'descripcion_puesto': new FormControl('', Validators.required),
+'numero_vacantes': new FormControl('', Validators.required),
+'duracion': new FormControl('', Validators.required),
+'salario': new FormControl('', Validators.required),
+'horario': new FormControl('', Validators.required)
+});
+} 
 
   ngOnInit() {
   }
-
+ 
+  
 }
