307ef4854fcbb56d5804d513063fa70862bb87b5
Carolina Orellana <carol.orellana.vk@gmail.com>
2017-05-10 10:24:28 +0200

    Funcionalidad editar oferta

--- src/app/formulario-adm/formulario-adm.component.ts
+++ src/app/formulario-adm/formulario-adm.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { OfertaService } from '../oferta.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+import { Oferta } from '../oferta';
 
 @Component({
   selector: 'app-formulario-adm',
@@ -10,30 +12,84 @@ import { Router } from '@angular/router';
 })
 export class FormularioAdmComponent implements OnInit {
   miFormulario: FormGroup;
+  private subscripcion: Subscription;
+  private esNueva = true;
+  private oferta: Oferta;
+  private ofertaId: number;
 
-  constructor(private ofertaService: OfertaService, private router: Router) {this.miFormulario = new FormGroup({
-'imagenUrl': new FormControl('', Validators.required),
-'puesto': new FormControl('', Validators.required),
-'nombreEmpresa': new FormControl('', Validators.required),
-'lugar': new FormControl('', Validators.required),
-'estudios': new FormControl('', Validators.required),
-'experiencia': new FormControl('', Validators.required),
-'skills': new FormControl('', Validators.required),
-'descEmpresa': new FormControl('', Validators.required),
-'descripcion': new FormControl('', Validators.required),
-'vacantes': new FormControl('', Validators.required),
-'duracion': new FormControl('', Validators.required),
-'salario': new FormControl('', Validators.required),
-'horario': new FormControl('', Validators.required)
-});
-}
-
-  ngOnInit() {
+  constructor(private ofertaService: OfertaService, private router: Router, private activatedRoute: ActivatedRoute) {
 
   }
-  onSubmit() {
-     this.ofertaService.anadirOferta(this.miFormulario.value);
-     this.router.navigate(['lista-ofertas']);
 
+  ngOnInit() {
+    this.subscripcion = this.activatedRoute.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {
+          this.esNueva = false;
+          this.ofertaId = params['id'];
+          this.oferta = this.ofertaService.devolverOferta(this.ofertaId);
+        } else {
+          this.esNueva = true;
+          this.oferta = null;
+        }
+        this.inicializarFormulario();
+      }
+    );
+  }
+  onSubmit() {
+    this.ofertaService.anadirOferta(this.miFormulario.value);
+    this.router.navigate(['lista-ofertas']);
+
+  }
+  private inicializarFormulario() {
+    let imagenOferta = '';
+    let puestoOferta = '';
+    let nombreEmpresaOferta = '';
+    let lugarOferta = '';
+    let estudiosOferta = '';
+    let experienciaOferta = '';
+    let skillsOferta: FormArray = new FormArray([]);
+    let desEmpresaOferta = '';
+    let descripOferta = '';
+    let vacantesOferta;
+    let duracionOferta = '';
+    let salarioOferta = '';
+    let horarioOferta = '';
+
+    if (!this.esNueva) {
+      for (let i = 0; i < this.oferta.skills.length; i++) {
+        skillsOferta.push(new FormControl(this.oferta.skills[i], Validators.required)
+
+        );
+      }
+      imagenOferta = this.oferta.imagenUrl;
+      puestoOferta = this.oferta.puesto;
+      nombreEmpresaOferta = this.oferta.nombreEmpresa;
+      lugarOferta = this.oferta.lugar;
+      estudiosOferta = this.oferta.estudios;
+      experienciaOferta = this.oferta.experiencia;
+      desEmpresaOferta = this.oferta.descEmpresa;
+      descripOferta = this.oferta.descripcion;
+      vacantesOferta = this.oferta.vacantes;
+      duracionOferta = this.oferta.duracion;
+      salarioOferta = this.oferta.salario;
+      horarioOferta = this.oferta.horario;
+    }
+
+    this.miFormulario = new FormGroup({
+      'imagenUrl': new FormControl(imagenOferta, Validators.required),
+      'puesto': new FormControl(puestoOferta, Validators.required),
+      'nombreEmpresa': new FormControl(nombreEmpresaOferta, Validators.required),
+      'lugar': new FormControl(lugarOferta, Validators.required),
+      'estudios': new FormControl(estudiosOferta, Validators.required),
+      'experiencia': new FormControl(experienciaOferta, Validators.required),
+      'skills': new FormControl(skillsOferta, Validators.required),
+      'descEmpresa': new FormControl(desEmpresaOferta, Validators.required),
+      'descripcion': new FormControl(descripOferta, Validators.required),
+      'vacantes': new FormControl(vacantesOferta, Validators.required),
+      'duracion': new FormControl(duracionOferta, Validators.required),
+      'salario': new FormControl(salarioOferta, Validators.required),
+      'horario': new FormControl(horarioOferta, Validators.required)
+    });
   }
 }
